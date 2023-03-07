1f19fbd3234920b1e5221dc551e57c823aaac58e
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-03-07 15:58:10 -0800

    task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@
 for number in range(100):
     if number != 99:
         print("{:02d}".format(number), end=', ')
-     else:
-         print("{:02d}".format(number))
+    else:
+        print("{:02d}".format(number))
