a0a81ef79240c60cf709644115692ff371910f89
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-03-08 22:18:23 -0800

    task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+
+def main():
+    length = len(argv) - 1
+    word = "arguments"
+
+    if length == 0:
+        print(f"{length} {word}.")
+    elif length != 1:
+        print(f"{length} {word}:")
+    else:
+        word = "argument"
+        print(f"{length} {word}:")
+
+    if length >= 1:
+        for count, args in enumerate(argv):
+            if count == 0:
+                continue
+            print(f"{count}: {args}")
+
+if __name__ == '__main__':
+    main()
