1d731e920ac22a6720d7dede23fe052bf23ff443
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-03-11 08:54:48 -0800

    task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b = len(tuple_b)
 
-    if len_a == 0:if len_a == 0:
+    if len_a == 0:
         a1 = 0
         a2 = 0
     elif len_a == 1:
