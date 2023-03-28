2d657a865793138f37739ed1ea8d3da63d7f4ecc
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-03-28 15:58:15 -0700

    task

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Example Google style docstrings.
+
 This module demonstrates documentation as specified by the `Google Python
 Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
+
 Example:
     Examples can be given using either the ``Example`` or ``Examples``
     sections. Sections support any reStructuredText formatting, including
