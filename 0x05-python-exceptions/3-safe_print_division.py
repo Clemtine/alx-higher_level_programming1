fb610dcf6ce0ce8477b0a371098f743bcbe63606
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-03-29 03:06:59 -0700

    task

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / 
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
