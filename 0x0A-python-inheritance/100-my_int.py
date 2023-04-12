b281617b6bf7590f97c568a426baef537b322cb1
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-04-12 13:21:20 -0700

    task 0x0A H

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Class that inherits from class int"""
+
+class MyInt(int):
+    """ Class that inherits from class int"""
+
+    def __eq__(self, other):
+        """ Method that returns != check """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Method that returns == check """
+        return int.__eq__(self, other)
