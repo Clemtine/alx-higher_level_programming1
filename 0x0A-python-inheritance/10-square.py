dc376280fc8294f7d8f98f15a70ccb57ea4bb181
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-04-12 13:19:13 -0700

    task 0x0A H

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Class that defines a Square from Rectangle class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """ Class that defines a Square from Rectangle class """
+    def __init__(self, size):
+        """ Method that initializes a Square """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Method that returns a string with the area """
+        return super().area()
