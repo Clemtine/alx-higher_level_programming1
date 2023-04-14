f99827c38119cad414dcca408e24119242976a1b
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-04-14 01:56:38 -0700

    task 0x0B

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that executes a function that appends a line """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """ Function that appends a new line when a string is found
 
