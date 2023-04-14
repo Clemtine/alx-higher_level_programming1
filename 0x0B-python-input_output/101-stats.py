443ee0b1bbf95b6867287850ab2b3dea9dc55611
Clementine AMEVOR <clementine.amevor97@gmail.com>
2023-04-14 03:38:00 -0700

    task 0x0B

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,6 +2,7 @@
 """ Module to print status code """
 import sys
 
+
 class Magic:
     """ Class to generates instances with dict and size"""
     def __init__(self):
@@ -9,7 +10,7 @@ class Magic:
         self.dic = {}
         self.size = 0
 
-    def init_dic(self):def init_dic(self):
+    def init_dic(self):
         """ Initialize dict """
         self.dic['200'] = 0
         self.dic['301'] = 0
@@ -32,24 +33,25 @@ class Magic:
             if self.dic[key] is not 0:
                 print("{}: {:d}".format(key, self.dic[key]))
 
-    if __name__ == "__main__":
-        magic = Magic()
-        magic.init_dic()
-        nlines = 0
 
-        try:
-            for line in sys.stdin:
-                if nlines % 10 == 0 and nlines is not 0:
-                    magic.print_info()
+if __name__ == "__main__":
+    magic = Magic()
+    magic.init_dic()
+    nlines = 0
 
-                try:
-                    list_line = [x for x in line.split(" ") if x.strip()]
-                    magic.add_status_code(list_line[-2])
-                    magic.size += int(list_line[-1].strip("\n"))
-                except:
-                    pass
-                nlines += 1
-            except KeyboardInterrupt:
+    try:
+        for line in sys.stdin:
+            if nlines % 10 == 0 and nlines is not 0:
                 magic.print_info()
-                raise
-            magic.print_info()
+
+            try:
+                list_line = [x for x in line.split(" ") if x.strip()]
+                magic.add_status_code(list_line[-2])
+                magic.size += int(list_line[-1].strip("\n"))
+            except:
+                pass
+            nlines += 1
+    except KeyboardInterrupt:
+        magic.print_info()
+        raise
+    magic.print_info()
